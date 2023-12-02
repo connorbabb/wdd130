21257dea64f54c9f4b7f4df63e7b4f5ba9928e1b
connorbabb <145483698+connorbabb@users.noreply.github.com>
2023-12-01 23:12:37 -0700

    web_component

--- haloterminal/forge.js
+++ haloterminal/forge.js
@@ -18,3 +18,15 @@ function openCity(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
   }
+
+function openFirst(evt, cityName){
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+}  
